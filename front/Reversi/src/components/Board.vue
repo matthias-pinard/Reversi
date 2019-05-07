6bf547326c715832265dd54dfeea67d66f7777c0
Squlalah <16013153@e008m09.istic.univ-rennes1.fr>
2019-05-07 11:17:07 +0200

    Création component Board

---
+++ front/Reversi/src/components/Board.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="board-container">
+        <div class="board">
+            <!--
+            <div 
+                class="cell"
+                v-for="cell in cells"
+                :class="{ playable: cell.isPlayable }"
+                :key="cell.key"
+                @click="play(cell)"
+            >
+                <piece
+                    v-if="cell.value !== 0"
+                    :color="cell.value"
+                />
+            </div>
+            -->
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+    computed: {
+        ...mapState([
+            'board'
+        ]),
+        ...mapGetters([
+            'playableCells'
+        ]),
+        cells () {
+            const { playableCells } = this;
+            return this.board.map((cell, index) => ({
+                key: 'cell-${index}',
+                value: cell,
+                position: index,
+                isPlayable: playableCells.indexOf(index) > -1
+            }));
+        }
+    },
+
+   /*methods: {
+        play (cell) {
+            if (cell.isPlayable) {
+                this.$store.dispatch('play', cell.position);
+            }
+        }
+   }*/
+}
+</script>
+
+
+<style src="@/style/board.css"></style>
